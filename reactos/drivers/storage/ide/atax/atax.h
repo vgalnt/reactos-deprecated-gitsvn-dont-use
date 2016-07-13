13a79dc9d0c832947dc6b885f99e544d6c1c7589
vgalnt <vgal@rambler.ru>
2016-07-14 02:47:37 +0800

    [ATAX] Add structure _COMMON_ATAX_DEVICE_EXTENSION and stub for structure _FDO_CHANNEL_EXTENSION.

--- reactos/drivers/storage/ide/atax/atax.h
+++ reactos/drivers/storage/ide/atax/atax.h
@@ -6,6 +6,23 @@
 #include <wdm.h>
 
 
+typedef struct _COMMON_ATAX_DEVICE_EXTENSION { 
+
+  PDEVICE_OBJECT  LowerDevice; // Если ниже в стеке есть Filter DO, то указатель на Filter DO, если нет, то указатель на нижний PDO. (Только для FDO. Для PDO будет NULL)
+  PDEVICE_OBJECT  LowerPdo;    // Указатель на нижний (в стеке) объект PDO
+  PDRIVER_OBJECT  SelfDriver;  // Указатель на собственный объект драйвера (на AtaX)
+  PDEVICE_OBJECT  SelfDevice;  // Указатель на собственный объект устройства (PDO либо FDO)
+  BOOLEAN         IsFDO;       // TRUE - если это расширение FDO, FALSE - PDO
+
+} COMMON_ATAX_DEVICE_EXTENSION, *PCOMMON_ATAX_DEVICE_EXTENSION;
+
+typedef struct _FDO_CHANNEL_EXTENSION {                   //// FDO расширение AtaXChannel
+
+  COMMON_ATAX_DEVICE_EXTENSION   CommonExtension;          // Общее и для PDO и для FDO расширений
+
+
+} FDO_CHANNEL_EXTENSION, *PFDO_CHANNEL_EXTENSION;
+
 //
 // Определения функций
 //
