a27b19e9665191a588801135e2b0dc6f2e0cfc4c
vgalnt <vgal@rambler.ru>
2016-07-14 02:31:02 +0800

    [ATAX] Stabs for AtaXUnload(), MajorFunction: IRP_MJ_CREATE, IRP_MJ_CLOSE, IRP_MJ_POWER, IRP_MJ_SYSTEM_CONTROL.

--- reactos/drivers/storage/ide/atax/atax.c
+++ reactos/drivers/storage/ide/atax/atax.c
@@ -4,6 +4,25 @@
 #include <debug.h>
 
 
+DRIVER_UNLOAD AtaXUnload;
+VOID NTAPI 
+AtaXUnload(PDRIVER_OBJECT DriverObject)
+{
+  DPRINT1("ATAX Unload FIXME \n");
+}
+
+DRIVER_DISPATCH AtaXCompleteIrp;
+NTSTATUS NTAPI
+AtaXCompleteIrp(
+    IN PDEVICE_OBJECT DeviceObject,
+    IN PIRP Irp)
+{
+  //DPRINT("AtaXCompleteIrp\n");
+  Irp->IoStatus.Status = STATUS_SUCCESS;
+  IoCompleteRequest(Irp, IO_NO_INCREMENT);
+  return STATUS_SUCCESS;
+}
+
 NTSTATUS NTAPI
 DriverEntry(
     IN PDRIVER_OBJECT DriverObject,
@@ -12,15 +31,15 @@ DriverEntry(
   DPRINT1("ATAX DriverEntry(%p '%wZ')\n", DriverObject, RegistryPath);
 
   DriverObject->DriverExtension->AddDevice = 0;//AddChannelFdo;
-  DriverObject->DriverUnload               = 0;//AtaXUnload;
+  DriverObject->DriverUnload               = AtaXUnload;
   DriverObject->DriverStartIo              = 0;//AtaXStartIo;
 
-  DriverObject->MajorFunction[IRP_MJ_CREATE]                  = 0;//AtaXCompleteIrp;
-  DriverObject->MajorFunction[IRP_MJ_CLOSE]                   = 0;//AtaXCompleteIrp;
+  DriverObject->MajorFunction[IRP_MJ_CREATE]                  = AtaXCompleteIrp;
+  DriverObject->MajorFunction[IRP_MJ_CLOSE]                   = AtaXCompleteIrp;
   DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL]          = 0;//AtaXDispatchDeviceControl;
   DriverObject->MajorFunction[IRP_MJ_INTERNAL_DEVICE_CONTROL] = 0;//AtaXDispatchScsi;          // IRP_MJ_SCSI == IRP_MJ_INTERNAL_DEVICE_CONTROL
-  DriverObject->MajorFunction[IRP_MJ_POWER]                   = 0;//AtaXCompleteIrp;           // AtaXDispatchPower;
-  DriverObject->MajorFunction[IRP_MJ_SYSTEM_CONTROL]          = 0;//AtaXCompleteIrp;           // AtaXDispatchSystemControl;
+  DriverObject->MajorFunction[IRP_MJ_POWER]                   = AtaXCompleteIrp;           // AtaXDispatchPower;
+  DriverObject->MajorFunction[IRP_MJ_SYSTEM_CONTROL]          = AtaXCompleteIrp;           // AtaXDispatchSystemControl;
   DriverObject->MajorFunction[IRP_MJ_PNP]                     = 0;//AtaXDispatchPnp;
 
   DPRINT1("ATAX DriverEntry: return STATUS_SUCCESS\n" );
