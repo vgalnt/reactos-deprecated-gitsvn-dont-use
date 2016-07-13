8e27f1e0187ba0cb591364885a578e7cdb1a58e7
vgalnt <vgal@rambler.ru>
2016-07-14 01:19:02 +0800

    [ATAX] First fills.

---
+++ reactos/drivers/storage/ide/atax/atax.c
@@ -0,0 +1,6 @@
+#include "atax.h"               
+
+#define NDEBUG
+#include <debug.h>
+
+
---
+++ reactos/drivers/storage/ide/atax/atax.h
@@ -0,0 +1,9 @@
+#ifndef _ATAX_PCH_
+#define _ATAX_PCH_
+
+#include <ntddk.h>
+#include <stdio.h>
+#include <wdm.h>
+
+
+#endif /* _ATAX_PCH_ */
