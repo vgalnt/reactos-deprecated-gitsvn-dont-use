d451942ba0a7254321a4b3f3ec6d16bbb400e297
vgalnt <vgal@rambler.ru>
2016-07-18 01:54:36 +0800

    [IDE] Bus master interface.

--- reactos/drivers/storage/ide/bmaster.h
+++ reactos/drivers/storage/ide/bmaster.h
@@ -2,6 +2,28 @@
 #define _BMASTER_PCH_
 
 
+typedef NTSTATUS
+(NTAPI *PBUS_MASTER_PREPARE)(
+  IN PVOID           DeviceExtension,
+  IN PVOID           CurrentVirtualAddress,
+  IN ULONG           Length,
+  IN PMDL            Mdl,
+  IN BOOLEAN         WriteToDevice,
+  IN PVOID           AllocateAdapter,
+  IN PDEVICE_OBJECT  AtaXChannelFdo);
+
+typedef NTSTATUS
+(NTAPI *PBUS_MASTER_START)(IN PVOID  DeviceExtension);
+
+typedef NTSTATUS
+(NTAPI *PBUS_MASTER_STOP)(IN PVOID  DeviceExtension);
+
+typedef NTSTATUS
+(NTAPI *PBUS_MASTER_READ_STATUS)(IN PVOID  DeviceExtension);
+
+typedef NTSTATUS
+(NTAPI *PBUS_MASTER_COMPLETE)(IN PVOID  DeviceExtension);
+
 typedef struct _BUS_MASTER_INTERFACE {
 
   // generic interface header
@@ -14,8 +36,12 @@ typedef struct _BUS_MASTER_INTERFACE {
   // bus master interface
   PVOID                    ChannelPdoExtension;
   ULONG                    BusMasterBase;
+  PBUS_MASTER_PREPARE      BusMasterPrepare;
+  PBUS_MASTER_START        BusMasterStart;
+  PBUS_MASTER_STOP         BusMasterStop;
+  PBUS_MASTER_READ_STATUS  BusMasterReadStatus;
+  PBUS_MASTER_COMPLETE     BusMasterComplete;
 
 } BUS_MASTER_INTERFACE, *PBUS_MASTER_INTERFACE;
-
 
 #endif /* _BMASTER_PCH_ */
