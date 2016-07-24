5430b061c885f0c74aa0c8fd72b8d51e6a016fba
vgalnt <vgal@rambler.ru>
2016-07-24 17:05:47 +0800

    [AHCIX] AhciSendCommand().

--- reactos/drivers/storage/ide/ahcix/ahcixpdo.c
+++ reactos/drivers/storage/ide/ahcix/ahcixpdo.c
@@ -277,6 +277,239 @@ AhciXStartChannel(
   return STATUS_SUCCESS;
 }
 
+NTSTATUS NTAPI   //  PAHCI_START_IO  AhciStartIo;
+AhciSendCommand(
+    IN PPDO_CHANNEL_EXTENSION ChannelPdoExtension,
+    IN PIDENTIFY_DATA IdentifyData,
+    IN PSCSI_REQUEST_BLOCK  Srb)
+{
+  PFDO_CONTROLLER_EXTENSION  ControllerFdoExtension;
+  AHCI_DEVICE_TYPE           DeviceType;
+  PAHCI_PORT_REGISTERS       Port;
+  PAHCI_COMMAND_HEADER       CommandHeader;
+  PAHCI_COMMAND_TABLE        CommandTable;
+  PHYSICAL_ADDRESS           PhCommandTable;
+  PHYSICAL_ADDRESS           PhDataBaseAddress;
+  PFIS_REGISTER_H2D          CommandFIS; // 0 Slot
+  ULONG                      InterruptStatus;
+  ULONG                      InterruptEnable;
+  ULONG                      AhciChannel;
+  BOOLEAN                    Write;
+  ULONG                      StartingSector;
+  USHORT                     SectorsPerTrack, Heads;
+  PCDB                       AtapiCommand;
+  ULONG                      Slot = 0;
+  NTSTATUS                   Status;
+
+  DPRINT("AhciSendCommand: ChannelPdoExtension - %p, Srb - %p\n", ChannelPdoExtension, Srb);
+  DPRINT("AhciSendCommand: Command %x to device %d\n", Srb->Cdb[0], Srb->TargetId);
+
+  ControllerFdoExtension = (PFDO_CONTROLLER_EXTENSION)ChannelPdoExtension->ControllerFdo->DeviceExtension;
+  DPRINT("AhciSendCommand: ControllerFdoExtension - %p\n", ControllerFdoExtension);
+
+  AhciChannel = ChannelPdoExtension->AhciInterface.AhciChannel;  // физический канал (0 ... 31)
+  DPRINT("AhciSendCommand: AhciChannel - %d\n", AhciChannel);
+
+  DeviceType = ControllerFdoExtension->DeviceType[AhciChannel];
+  DPRINT("AhciSendCommand: DeviceType - %x\n", DeviceType);
+
+  if ( DeviceType == AtaDrive )
+  {
+    DPRINT("AhciSendCommand: ATA device\n");
+
+    switch ( Srb->Cdb[0] )
+    {
+      case SCSIOP_READ:              /* 0x28 */
+      case SCSIOP_WRITE:             /* 0x2A */
+        DPRINT("AhciSendCommand / SCSIOP_READ or SCSIOP_WRITE\n");
+        break;
+
+      case SCSIOP_TEST_UNIT_READY:
+        DPRINT("AhciSendCommand / SCSIOP_TEST_UNIT_READY FIXME\n");
+        Status = SRB_STATUS_INVALID_REQUEST;
+        return Status;
+
+      default:
+        DPRINT("AhciSendCommand: Unsupported command FIXME%x\n", Srb->Cdb[0]);
+        Status = SRB_STATUS_INVALID_REQUEST;
+        return Status;
+    }
+    
+    SectorsPerTrack = IdentifyData->NumSectorsPerTrack;
+    Heads           = IdentifyData->NumHeads;
+
+    // начальный сектор извлекаем из CDB
+    StartingSector = ((PCDB)Srb->Cdb)->CDB10.LogicalBlockByte3       |
+                     ((PCDB)Srb->Cdb)->CDB10.LogicalBlockByte2 << 8  |
+                     ((PCDB)Srb->Cdb)->CDB10.LogicalBlockByte1 << 16 |
+                     ((PCDB)Srb->Cdb)->CDB10.LogicalBlockByte0 << 24;
+
+    DPRINT("AhciSendCommand: Starting sector is %x, Number of bytes %x\n", StartingSector, Srb->DataTransferLength);
+    DPRINT("AhciSendCommand: Cylinder %x Head %x Sector %x\n",
+            StartingSector  / (SectorsPerTrack  * Heads),
+            (StartingSector /  SectorsPerTrack) % Heads,
+            StartingSector  %  SectorsPerTrack + 1);
+  }
+  else if ( DeviceType == AtapiDrive )
+  {
+    /*
+    DPRINT("AhciAtapiSendCommand: Srb->Cdb[0]  - %x\n", Srb->Cdb[0]);
+    DPRINT("AhciAtapiSendCommand: Srb->Cdb[1]  - %x\n", Srb->Cdb[1]);
+    DPRINT("AhciAtapiSendCommand: Srb->Cdb[2]  - %x\n", Srb->Cdb[2]);
+    DPRINT("AhciAtapiSendCommand: Srb->Cdb[3]  - %x\n", Srb->Cdb[3]);
+    DPRINT("AhciAtapiSendCommand: Srb->Cdb[4]  - %x\n", Srb->Cdb[4]);
+    DPRINT("AhciAtapiSendCommand: Srb->Cdb[5]  - %x\n", Srb->Cdb[5]);
+    DPRINT("AhciAtapiSendCommand: Srb->Cdb[6]  - %x\n", Srb->Cdb[6]);
+    DPRINT("AhciAtapiSendCommand: Srb->Cdb[7]  - %x\n", Srb->Cdb[7]);
+    DPRINT("AhciAtapiSendCommand: Srb->Cdb[8]  - %x\n", Srb->Cdb[8]);
+    DPRINT("AhciAtapiSendCommand: Srb->Cdb[9]  - %x\n", Srb->Cdb[9]);
+    DPRINT("AhciAtapiSendCommand: Srb->Cdb[10] - %x\n", Srb->Cdb[10]);
+    DPRINT("AhciAtapiSendCommand: Srb->Cdb[11] - %x\n", Srb->Cdb[11]);
+    DPRINT("AhciAtapiSendCommand: Srb->Cdb[12] - %x\n", Srb->Cdb[12]);
+    DPRINT("AhciAtapiSendCommand: Srb->Cdb[13] - %x\n", Srb->Cdb[13]);
+    DPRINT("AhciAtapiSendCommand: Srb->Cdb[14] - %x\n", Srb->Cdb[14]);
+    DPRINT("AhciAtapiSendCommand: Srb->Cdb[15] - %x\n", Srb->Cdb[15]);
+    */
+
+    DPRINT("AhciSendCommand: ATAPI device\n");
+  }
+
+  if ( Srb->SrbFlags & SRB_FLAGS_DATA_IN )
+    Write = 0;
+  else if ( Srb->SrbFlags & SRB_FLAGS_DATA_OUT )
+    Write = 1;
+  else
+    return STATUS_UNSUCCESSFUL;
+
+  Port = ChannelPdoExtension->AhciInterface.AhciPortControl;
+  //DPRINT("AhciSendCommand: Port - %p\n", Port);
+
+  InterruptStatus = READ_REGISTER_ULONG((PULONG)&Port->InterruptStatus);
+  DPRINT("AhciSendCommand: InterruptStatus - %p\n", InterruptStatus);
+  WRITE_REGISTER_ULONG((PULONG)&Port->InterruptStatus, 0xFFFFFFFF);  // Clear pending interrupt bits
+
+  InterruptEnable = READ_REGISTER_ULONG((PULONG)&Port->InterruptEnable);
+  //DPRINT("AhciSendCommand: Port->InterruptEnable - %p\n", InterruptEnable);
+
+  InterruptEnable |= 1;
+  WRITE_REGISTER_ULONG((PULONG)&Port->InterruptEnable, InterruptEnable);
+  DPRINT("AhciSendCommand: Port->InterruptEnable - %p\n", READ_REGISTER_ULONG((PULONG)&Port->InterruptEnable));
+
+  CommandHeader = (PAHCI_COMMAND_HEADER)(
+                  (ULONG_PTR)ChannelPdoExtension->AhciInterface.CmdListBaseAddress +
+                  Slot * sizeof(AHCI_COMMAND_HEADER));
+
+  //DPRINT("AhciSendCommand: CommandHeader         - %p\n", CommandHeader);
+
+  CommandTable = (PAHCI_COMMAND_TABLE)
+                 ChannelPdoExtension->AhciInterface.AhciBuffer +
+                 Slot * sizeof(AHCI_COMMAND_TABLE);
+
+  PhCommandTable = MmGetPhysicalAddress((PVOID)CommandTable);
+
+  CommandHeader->DescriptionInformation.CommandFISLength = sizeof(FIS_REGISTER_H2D)/sizeof(ULONG);  // Command FIS size
+  CommandHeader->DescriptionInformation.Write            = Write;                                   // Write or Read
+  CommandHeader->DescriptionInformation.PRDTLength       = 1;                                       // 1 PRD (Physical Region Descriptor)
+
+  if ( DeviceType == AtaDrive )
+    CommandHeader->DescriptionInformation.ATAPI = 0;
+  else if ( DeviceType == AtapiDrive )
+    CommandHeader->DescriptionInformation.ATAPI = 1;
+
+  CommandHeader->PRDTByteCount           = Srb->DataTransferLength;
+  CommandHeader->CmdTableDescriptorBase  = (PAHCI_COMMAND_TABLE)PhCommandTable.LowPart;
+  CommandHeader->CmdTableDescriptorBaseU = PhCommandTable.HighPart;
+
+  PhDataBaseAddress = MmGetPhysicalAddress((PVOID)ChannelPdoExtension->AhciInterface.DmaBuffer);
+
+  CommandTable->PRDTable.Descriptor[0].DataBaseAddress       = PhDataBaseAddress.LowPart;
+  CommandTable->PRDTable.Descriptor[0].DataBaseAddressU      = PhDataBaseAddress.HighPart;
+  CommandTable->PRDTable.Descriptor[0].DataByteCount         = Srb->DataTransferLength;
+  CommandTable->PRDTable.Descriptor[0].InterruptOnCompletion = 1;
+
+  DPRINT("AhciSendCommand: Srb->DataBuffer         - %p\n", Srb->DataBuffer);
+  DPRINT("AhciSendCommand: Srb->DataTransferLength - %p\n", Srb->DataTransferLength);
+
+  if ( Write )
+    RtlMoveMemory((PVOID)ChannelPdoExtension->AhciInterface.DmaBuffer,
+                  (PVOID)Srb->DataBuffer, Srb->DataTransferLength);
+  else
+    RtlZeroMemory((PVOID)ChannelPdoExtension->AhciInterface.DmaBuffer,
+                  Srb->DataTransferLength);
+
+  // Setup CommandFIS
+  CommandFIS = ChannelPdoExtension->AhciInterface.CommandFIS;
+  RtlZeroMemory(CommandFIS, sizeof(FIS_REGISTER_H2D));
+  //ChannelPdoExtension->AhciInterface.CommandFIS = CommandFIS;
+
+  CommandFIS->FISType      = FIS_TYPE_REGISTER_H2D;
+  CommandFIS->Device       = 0;		                // Master device
+  CommandFIS->RegisterType = 1;		                // Write command register
+
+  if ( DeviceType == AtaDrive )
+  {
+    if ( Write )
+    {
+      CommandFIS->Command = IDE_COMMAND_WRITE_DMA;  // 0xCA
+    }
+    else
+    {
+      CommandFIS->Command = IDE_COMMAND_READ_DMA;  // 0xC8
+    }
+
+    CommandFIS->LBA0      = (UCHAR)((StartingSector % SectorsPerTrack) + 1);             // устанавливаем регистр LowLBA
+    CommandFIS->LBA1      = (UCHAR)(StartingSector  / (SectorsPerTrack * Heads));        // устанавливаем регистр MidLBA
+    CommandFIS->LBA2      = (UCHAR)((StartingSector / (SectorsPerTrack * Heads)) >> 8);  // устанавливаем регистр HighLBA
+    CommandFIS->Device    = (UCHAR)((StartingSector / SectorsPerTrack) % Heads);         // устанавливаем регистр DriveSelect
+    CommandFIS->CountLow  = (UCHAR)((Srb->DataTransferLength + 0x1FF) / 0x200);          // устанавливаем регистр SectorCount
+    CommandFIS->CountHigh = 0;
+  }
+  else if ( DeviceType == AtapiDrive )
+  {
+    AtapiCommand = (PCDB)&CommandTable->AtapiCommand;     //UCHAR  AtapiCommand[0x10]
+    RtlCopyMemory(AtapiCommand, (PVOID)Srb->Cdb, 0x10);   // FIXME: size CDB
+
+    CommandFIS->Command = IDE_COMMAND_ATAPI_PACKET;       // 0xA0
+    CommandFIS->FeaturesLow  = 1;                         // 1 - DMA, 0 - PIO
+
+    // устанавливаем количество байт пересылки в соотвктствующие регистры
+    CommandFIS->LBA0      = 0;                                                           // устанавливаем регистр LowLBA
+    CommandFIS->LBA1      = (UCHAR)(Srb->DataTransferLength & 0xFF);                     // устанавливаем регистр MidLBA
+    CommandFIS->LBA2      = (UCHAR)(Srb->DataTransferLength >> 8);                       // устанавливаем регистр HighLBA
+
+                                                          //if ( Srb->DataTransferLength >= 0x10000 )
+                                                          //  ByteCountLow = ByteCountHigh = 0xFF;
+    CommandFIS->CountLow  = 0;  //(UCHAR)(ByteCountLow);
+    CommandFIS->CountHigh = 0;  //(UCHAR)(ByteCountHigh);
+  }
+
+  /*
+  DPRINT(" ... : CommandFIS                     - %p\n", CommandFIS);
+  DPRINT(" ... : CommandFIS->PortMultiplierPort - %x\n", CommandFIS->PortMultiplierPort);
+  DPRINT(" ... : CommandFIS->RegisterType       - %x\n", CommandFIS->RegisterType);
+  DPRINT(" ... : CommandFIS->Command            - %x\n", CommandFIS->Command);
+  DPRINT(" ... : CommandFIS->LBA0               - %x\n", CommandFIS->LBA0);
+  DPRINT(" ... : CommandFIS->LBA1               - %x\n", CommandFIS->LBA1);
+  DPRINT(" ... : CommandFIS->LBA2               - %x\n", CommandFIS->LBA2);
+  DPRINT(" ... : CommandFIS->Device             - %x\n", CommandFIS->Device);
+  DPRINT(" ... : CommandFIS->LBA3               - %x\n", CommandFIS->LBA3);
+  DPRINT(" ... : CommandFIS->LBA4               - %x\n", CommandFIS->LBA4);
+  DPRINT(" ... : CommandFIS->LBA5               - %x\n", CommandFIS->LBA5);
+  DPRINT(" ... : CommandFIS->FeaturesLow        - %x\n", CommandFIS->FeaturesLow);
+  DPRINT(" ... : CommandFIS->FeaturesHigh       - %x\n", CommandFIS->FeaturesHigh);
+  DPRINT(" ... : CommandFIS->CountLow           - %x\n", CommandFIS->CountLow);
+  DPRINT(" ... : CommandFIS->CountHigh          - %x\n", CommandFIS->CountHigh);
+  DPRINT(" ... : CommandFIS->Control            - %x\n", CommandFIS->Control);
+  */
+
+  Port->CommandIssue = 1 << Slot;  // Issue command
+
+  // ждём аппаратное прерывание
+  Status = STATUS_SUCCESS;
+  DPRINT("AhciSendCommand: return - %p\n", Status);
+  return Status;
+}
+
 NTSTATUS
 AhciXPdoPnpDispatch(
     IN PDEVICE_OBJECT ChannelPdo,
