4625a8bc204e2cbfa26b62cfe19eb70e18b4b362
vgalnt <vgal@rambler.ru>
2016-07-24 12:56:57 +0800

    [AHCIX] DriverEntry().

--- reactos/drivers/storage/ide/ahcix/ahcix.c
+++ reactos/drivers/storage/ide/ahcix/ahcix.c
@@ -5,3 +5,25 @@
 #include <debug.h>
 
 
+NTSTATUS NTAPI
+DriverEntry(
+    IN PDRIVER_OBJECT DriverObject,
+    IN PUNICODE_STRING RegistryPath)
+{
+  DPRINT("AhciX DriverEntry (%p '%wZ')\n", DriverObject, RegistryPath);
+
+  DriverObject->DriverExtension->AddDevice                    = AhciXAddDevice;
+  DriverObject->DriverUnload                                  = AhciXUnload;
+  DriverObject->DriverStartIo                                 = 0;  // AhciXStartIo;
+
+  DriverObject->MajorFunction[IRP_MJ_CREATE]                  = AhciXForwardOrIgnore;
+  DriverObject->MajorFunction[IRP_MJ_CLOSE]                   = AhciXForwardOrIgnore;
+  DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL]          = AhciXForwardOrIgnore;  // AhciXDispatchDeviceControl;
+  DriverObject->MajorFunction[IRP_MJ_INTERNAL_DEVICE_CONTROL] = AhciXForwardOrIgnore;  // AhciXDispatchScsi;
+
+  DriverObject->MajorFunction[IRP_MJ_POWER]                   = AhciXForwardOrIgnore;  // AhciXDispatchPower;
+  DriverObject->MajorFunction[IRP_MJ_SYSTEM_CONTROL]          = AhciXForwardOrIgnore;  // AhciXDispatchSystemControl;
+  DriverObject->MajorFunction[IRP_MJ_PNP]                     = AhciXPnpDispatch;
+
+  return STATUS_SUCCESS;
+}
