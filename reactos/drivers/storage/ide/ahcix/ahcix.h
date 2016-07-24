104f226bbb42f06fe047f3b5c9d6d7a3990944c7
vgalnt <vgal@rambler.ru>
2016-07-24 12:50:16 +0800

    [AHCIX] Define structure _COMMON_DEVICE_EXTENSION.

--- reactos/drivers/storage/ide/ahcix/ahcix.h
+++ reactos/drivers/storage/ide/ahcix/ahcix.h
@@ -1,6 +1,15 @@
 #ifndef _AHCIX_PCH_
 #define _AHCIX_PCH_
 
+#include <ntifs.h>
+#include <scsi.h>
+#include <ide.h>
+#include <..\ahci.h>
+
+
+typedef struct _COMMON_DEVICE_EXTENSION {
+  BOOLEAN IsFDO;
+} COMMON_DEVICE_EXTENSION, *PCOMMON_DEVICE_EXTENSION;
 
 
 #endif /* _AHCIX_PCH_ */
